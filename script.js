689a73d9cd5b85f3afa9a791b9c789094ad5536b
Adelina Fatu <adelinamfatu@gmail.com>
2022-11-19 19:15:09 +0200

    Get json from eurostat

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 window.addEventListener("load", main);
 
+var indicators;
+var countries;
+var lifeExpectancies, population, GDP;
+
 async function main() 
 {
     await fetchData();
     populateSelect();
+    await getJSONData();
 }
 
 var jsondata;
 async function fetchData()
 {
-    await fetch("./media/eurostat.json")
+    jsondata = await fetch("./media/eurostat.json")
         .then((response) => response.json())
-        .then((data) => { jsondata = data });
+        .then((data) => { return data; });
 }
 
 async function populateSelect()
 {
     var indicatorSelect = document.getElementById("indicator-dropdown");
 
-    var indicators = new Set(jsondata?.map(i => i.indicator));
+    indicators = new Set(jsondata?.map(i => i.indicator));
     for(const indicator of indicators)
     {
         var option = document.createElement("option");
@@ -29,7 +34,7 @@ async function populateSelect()
 
     var countrySelect = document.getElementById("country-dropdown");
 
-    var countries = new Set(jsondata?.map(c => c.tara));
+    countries = new Set(jsondata?.map(c => c.tara));
     for(const country of countries)
     {
         var option = document.createElement("option");
@@ -37,4 +42,32 @@ async function populateSelect()
         option.value = country;
         countrySelect.add(option);
     }
+}
+
+async function getJSONData()
+{
+    var linkLifeExpectancy, linkPopulation, linkGDP;
+    for(const country of countries)
+    {
+        linkLifeExpectancy = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/demo_mlexpec?sex=T&age=Y1&sinceTimePeriod=2006";
+        linkPopulation = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/demo_pjan?sex=T&age=TOTAL&sinceTimePeriod=2006";
+        linkGDP = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/sdg_08_10?na_item=B1GQ&unit=CLV10_EUR_HAB&sinceTimePeriod=2006";
+        linkLifeExpectancy += "&geo=" + country + "?format=JSON";
+        linkPopulation += "&geo=" + country + "?format=JSON";
+        linkGDP += "&geo=" + country + "?format=JSON";
+        lifeExpectancies = await fetch(linkLifeExpectancy)
+        .then((response) => response.json())
+        .then((data) => { return data });
+        population = await fetch(linkPopulation)
+        .then((response) => response.json())
+        .then((data) => { return data });
+        GDP = await fetch(linkGDP)
+        .then((response) => response.json())
+        .then((data) => { return data });
+    }
+}
+
+function showEvolutionGraphic()
+{
+    
 }
